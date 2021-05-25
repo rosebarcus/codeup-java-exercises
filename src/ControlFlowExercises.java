9b3ff5e36cfac59cbdfa6890bb3cce82791a60a9
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-25 15:39:36 -0500

    exercise 2.2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int j = 2;
+        int j = 0;
         do {
-            System.out.println(j*2);
-            j+= j;
+            System.out.println(j * 2);
+            j++;
 
-        } while (j < 1000000);
+        } while (j <= 50);
     }
 }
