4843908176633e54af60abe23970fa04a52157cb
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-25 14:43:49 -0500

    exercise 2.1 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+        int j = 2;
+        do {
+            if (j % 2 ==0){
+                System.out.println(j + "\n");
 
-    }
-}
+            }
+            j++;
+    } while (j < 100);
+} }
