5a1b33ba8214d78f9480b4188c10763753a80a68
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-26 10:10:35 -0500

    almost complete javier bonus question, missing functionality at the end

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,14 +5,22 @@ public class ControlFlowExercises {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("What number would you like to go up to?");
+        boolean userWantsToContinue = false;
+        do {
         int userInput = scanner.nextInt();
         System.out.println("Here is your table!");
         System.out.println("");
         System.out.println("number | squared | cubed");
         System.out.println("______ | _______ | _____");
-        for(int i = 1; i <= userInput; i++){
+        for(int i = 1; i <= userInput; i++) {
             System.out.printf("%-7d| %-8d| %-5d%n ", i, i * i, i * i * i);
         }
+            System.out.println("Enter another number? y/n?");
+            String userAnswer = scanner.next();
+            if (userAnswer.equals("y")){
+                userWantsToContinue = true;
+            }
+        } while (userWantsToContinue);
 
     }
 }
