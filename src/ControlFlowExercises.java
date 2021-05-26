62ef6af9323f8171c9ee6a670904bfca029577b3
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-26 09:39:45 -0500

    refactored do while loop to for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,8 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long i = 2;
-        do {
-            System.out.println(i);
-            i *= i;
-        } while (i < 1000000);
 
+        for (int i = 100; i >= -10; i -=5){
+            System.out.println(i);
+        }
     }
 }
