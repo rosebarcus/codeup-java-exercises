2bb345aab7d2b0c9b44efcb9861489580bf201db
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-26 09:34:26 -0500

    last do while exercise solution

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,11 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        // Loops do while solution b
-        int i = 100;
-        do{
+        long i = 2;
+        do {
             System.out.println(i);
-            i-= 2;
-        } while (i >= -10);
+            i *= i;
+        } while (i < 1000000);
 
     }
 }
