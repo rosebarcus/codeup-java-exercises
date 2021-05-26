7985e2b73358b9e03635e2822b60328137ccc620
Rose Barcus <angela.rose.barcus@gmail.com>
2021-05-26 09:29:25 -0500

    solution for 1 b from walkthrough

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,11 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            if (i < 15) {
-                System.out.print(i + " ");
-            } else {
-                System.out.println(i);
-            }
-            i++;
-        }
+        // Loops do while solution b
+        int i = 0;
+        do{
+            System.out.println(i);
+            i+= 2;
+        } while (i <= 100);
 
     }
 }
