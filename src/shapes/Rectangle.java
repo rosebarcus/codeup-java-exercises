185335705aa63152b994d2250cf802ec1433100a
Rose Barcus <angela.rose.barcus@gmail.com>
2021-06-04 16:54:28 -0500

    WIP Interfaces and Abstract Classes exercises, up to step 7

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,6 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable {
 
-    protected double length;
-    protected double width;
 
-    public Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
-    }
-
-    public double getArea(){
-        return length * width;
-    }
-
-    public double getPerimeter(){
-        return 2 * length + 2 * width;
-
-    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,14 +1,8 @@
 package shapes;
 
-public class Square extends Rectangle{
-    public Square(double side){
-        super(side, side);
-    }
-    public double getArea(){
-        return length * length;
-    }
+public class Square extends Quadrilateral{
 
-    public double getPerimeter() {
-        return 4 * length;
-    }
+
+    Measurable myShape = new Square();
+
 }
